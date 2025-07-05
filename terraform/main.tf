8bdcd0b30ac21652de5d54f34936fe31e6a48153
aheissa <Ahmad.y.eissa@gmail.com>
2025-07-06 00:53:45 +0300

    2 VPC structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,9 @@ module "sample_vpc" {
   aws_availability_zone2 = var.aws_availability_zone2
 
   # ---  Step 4: Enable VPC Traffic Visibility  ---
-   mcd_dns_query_log_config_id       = module.cisco_mcd.mcd_dns_query_log_config_id
-   mcd_s3_bucket                     = module.cisco_mcd.mcd_s3_bucket
-   mcd_controller_aws_account_number = var.mcd_controller_aws_account_number
+  # mcd_dns_query_log_config_id       = module.cisco_mcd.mcd_dns_query_log_config_id
+  # mcd_s3_bucket                     = module.cisco_mcd.mcd_s3_bucket
+  # mcd_controller_aws_account_number = var.mcd_controller_aws_account_number
 
   # ---  Step 6: Secure Sample VPC  ---
   # mcd_service_vpc_id     = module.cisco_mcd.mcd_service_vpc_id
@@ -16,12 +16,12 @@ module "sample_vpc" {
 }
 
 # --- Step 2: Onboard with Cisco Multicloud Defense ---
- module "cisco_mcd" {
-   source                            = "./cisco_mcd"
-   mcd_deployment_name               = var.mcd_deployment_name
-   mcd_controller_aws_account_number = var.mcd_controller_aws_account_number
-   mcd_cloud_account_name            = var.mcd_cloud_account_name
-   aws_ssh_key_pair_name             = var.aws_ssh_key_pair_name
-   aws_availability_zone1             = var.aws_availability_zone1
-   aws_availability_zone2             = var.aws_availability_zone2
- }
+# module "cisco_mcd" {
+#   source                            = "./cisco_mcd"
+#   mcd_deployment_name               = var.mcd_deployment_name
+#   mcd_controller_aws_account_number = var.mcd_controller_aws_account_number
+#   mcd_cloud_account_name            = var.mcd_cloud_account_name
+#   aws_ssh_key_pair_name             = var.aws_ssh_key_pair_name
+#   aws_availability_zone1             = var.aws_availability_zone1
+#   aws_availability_zone2             = var.aws_availability_zone2
+# }
