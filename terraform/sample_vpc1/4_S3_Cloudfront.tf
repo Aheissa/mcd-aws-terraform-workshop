d220c65eb2599022b020bc0be84f7364d629de65
aheissa <Ahmad.y.eissa@gmail.com>
2025-07-07 12:43:24 +0300

    Updated s3 and cloud front

--- terraform/sample_vpc1/4_S3_Cloudfront.tf
+++ terraform/sample_vpc1/4_S3_Cloudfront.tf
@@ -81,12 +81,35 @@ resource "aws_cloudfront_distribution" "website_cdn" {
     origin_id                = "s3-website"
     origin_access_control_id = aws_cloudfront_origin_access_control.website_oac.id
   }
+  origin {
+    domain_name = aws_lb.sample_alb.dns_name
+    origin_id   = "us-alb"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
   enabled             = true
   default_root_object = "index.html"
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "s3-website"
+    viewer_protocol_policy = "allow-all"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+  ordered_cache_behavior {
+    path_pattern     = "/alb/*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "us-alb"
     viewer_protocol_policy = "allow-all"
     forwarded_values {
       query_string = false
