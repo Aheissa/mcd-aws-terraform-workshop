bb0b91abcfc5dc058fd164b2f0ca53478ef096c3
aheissa <Ahmad.y.eissa@gmail.com>
2025-07-12 11:49:07 +0300

    Pilot_testing_output

--- terraform/sample_vpc1/outputs.tf
+++ terraform/sample_vpc1/outputs.tf
@@ -1,31 +1,31 @@
 output "public_alb_dns_name" {
-  value = module.sample_vpc.public_alb_dns_name
+  value = aws_lb.sample_alb_public.dns_name
 }
 
 output "private_alb_dns_name" {
-  value = module.sample_vpc.private_alb_dns_name
+  value = aws_lb.sample_alb_private.dns_name
 }
 
 output "cloudfront_distribution_domain_name" {
-  value = module.sample_vpc.cloudfront_distribution_domain_name
+  value = aws_cloudfront_distribution.website_cdn.domain_name
 }
 
 output "nlb_dns_name" {
-  value = module.sample_vpc.nlb_dns_name
+  value = aws_lb.public_nlb.dns_name
 }
 
 output "ec2_instance1_public_dns" {
-  value = module.sample_vpc.ec2_instance1_public_dns
+  value = aws_instance.app_instance1.public_dns
 }
 
 output "ec2_instance1_public_ip" {
-  value = module.sample_vpc.ec2_instance1_public_ip
+  value = aws_instance.app_instance1.public_ip
 }
 
 output "ec2_instance2_public_dns" {
-  value = module.sample_vpc.ec2_instance2_public_dns
+  value = aws_instance.app_instance2.public_dns
 }
 
 output "ec2_instance2_public_ip" {
-  value = module.sample_vpc.ec2_instance2_public_ip
+  value = aws_instance.app_instance2.public_ip
 }
